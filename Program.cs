4cbd103fbafd121a37d22be5500733573703b9ed
chuck francis <chuck_francis10@yahoo.com>
2020-09-23 13:03:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SplittingASentanceIntoIndividualWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "I like cheese";
+
+            string[] words = s.Split(' ');
+
+            // loop through all the elements
+            for (int i = 0; i < words.Length; i++)
+            {
+                // print the word at element index i
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
